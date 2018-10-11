0af4a65344c9703debf28b3922dc5bff31986b90
Simone Bussoni <simbus82@gmail.com>
2018-10-11 18:26:05 +0200

    Update components/ProductOverview/ProductOverview.js

--- components/ProductOverview/ProductOverview.js
+++ components/ProductOverview/ProductOverview.js
@@ -113,7 +113,7 @@ class ProductOverview extends React.Component {
 }
 
 export const FILTERED_PRODUCTS = gql`
-query allFilteredProducts($collection: [id], $series: [id], $target: [id], $frame: [id], $shape: [id], $type: [id]) {
+query allFilteredProducts($collection: [ID], $series: [ID], $target: [ID], $frame: [ID], $shape: [ID], $type: [ID]) {
 allEyewears(filter: {
     collection: {in: $collection},
     series: {in: $series},
